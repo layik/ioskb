05e80e46d8bfe29b51ac1bbe2ad59380e539b74e
Layik Hama <layik.hama@gmail.com>
2019-04-02 22:16:43 +0100

    custom keyboard height is still 151

---
+++ keyboard/KeyboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  KeyboardViewController.h
+//  keyboard
+//
+//  Created by layik on 02/04/2019.
+//  Copyright © 2019 layik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KeyboardViewController : UIInputViewController
+
+@end
